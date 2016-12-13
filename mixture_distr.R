63f024c9fad82a1fbd985b78c227384ec2feb463
Pedro Costa <pedro.mpm.costa@gmail.com>
2016-12-13 08:00:27 +0000

    renamed functions removed implementation KS-test added implementation distr. function

--- mixture_distr.R
+++ mixture_distr.R
@@ -16,24 +16,27 @@ cdf <- function(x, w1, a, b, lambda) {
   w1 * punif(x, min=a, max=b) + (1-w1) * pexp(x, rate=lambda)
 }
 
-
 #
 # U ~ F(X) => X ~ F-1(U)
 # U ~ U[0,1]
-# X ~ p1*f1(u) + p2*f2(u)
+# X ~ p1*F1^-1(U) + p2*F2^-1(U)
 #
-gen.mix.distr <- function(nvals, p1, a, b, lambda) {
-  X <- NULL
+gen.mix.sample <- function(nvals, p1, a, b, lambda) {
+  # Generate variable U ~ U[0,1]
   U <- runif(nvals)
-
-  p2 <- 1-p1
+  X <- NULL
+  
   n  <- 0
+  p2 <- 1-p1
   while(n < nvals) {
     pn <- runif(1)
     n <- n + 1
+    # Choose j in {1, ..., r} according to p1, ..., pr
     if(pn < p2) {
-      X[n] <- -log(U[n])/lambda
+      # Generate Xi with pdf fi(x)
+      X[n] <- -log(U[n])/lambda 
     } else {
+      # Generate Xi with pdf fi(x)
       X[n] <- U[n]*(b-a)+a
     }
   }
@@ -41,25 +44,49 @@ gen.mix.distr <- function(nvals, p1, a, b, lambda) {
 }
 
 #
+# Fu^-1(U)
+#
+# gen.mix.distr <- function(X,p1,a,b,lambda) {
+#   FX <- rep(0,length(X))
+#   
+#   for(i in seq_along(X)) {
+#     x <- X[i]
+#     if(x>b) { # x > b 
+#       FX[i] <- 1 - (1-p1)*exp(-2*x)
+#     } 
+#     else if(x>0) { # 0≤x<b
+#       FX[i] <- 0.7+ (p1*x)/(b-a) - (1-p1)*exp(-lambda*x) 
+#     } 
+#     else if(x>a) { # a<x≤0
+#       FX[i] <- 0.3 + (p1*x)/(b-a)
+#     } 
+#     else { # x<a
+#       #  FX[i] <- 0
+#     }
+#   }
+#   FX
+# }
+
+#
 # Plot the distributions
 #
-plot.mix.distr <- function(XX, fx, gx, a, b, lambda, breaks=50) {
+plot.mix.sample <- function(X, x.axis, fx, a, b, lambda, breaks=50) {
   
-  x.min <- min(XX); y.min <- 0
-  x.max <- max(XX); y.max <- 1 
+  x.min <- min(x.axis); y.min <- 0
+  x.max <- max(x.axis); y.max <- 1 
   
   title <- sprintf("Composition method to add U[%d,%d] and Exp(%d)",
                    a, b, lambda)
   
   par(cex.axis=0.8, pty="s")
-  hist(gx, probability=TRUE, breaks=breaks,
+  hist(X, probability=TRUE, breaks=breaks,
        xlim=c(x.min,x.max), ylim=c(y.min,y.max),
        xlab="x", ylab="f(x)", main=title)
-  lines(XX, fx, col='red')
+  lines(x.axis, fx, col='red')
 }
 
 #
-#
+# Quantile-quantile plot
 #
 qqplot <- function(nvals, X, Y) {
   q <- seq(0.5/nvals, 1 , 1/nvals)
@@ -68,38 +95,12 @@ qqplot <- function(nvals, X, Y) {
   qq.popY <- quantile(sort(Y), prob=q)
   qq <- cbind(qq.popX, qq.popY)
   
-  plot(qq,pch=".", xlim=c(-3,3), ylim=c(-3,3), 
+  plot(qq,pch=".", xlim=c(0,1), ylim=c(0,1), 
        xlab="X", ylab="Y", main="QQ plot")
   abline(0,1,col="red")
 }
 
-#
-# Kolmogorov-Smirnoff test
-#
-ks.mix.distr <- function(A,B,alpha=0.01) {
-  q  <- seq(0, 1, 0.01)
-  sA <- quantile(A, prob=q)
-  sB <- quantile(B, prob=q)
-  T <- rbind(cbind(X=sA,set=1,FnA=0,FnB=0,DFn=0),
-             cbind(X=sB,set=2,FnA=0,FnB=0,DFn=0))
-  
-  T <- T[order(T[,'X']),]
-  N <- length(q)
-  for(n in seq(2*N)) {
-    T[n,'FnA'] <- sum(T[1:n,'set']==1)/N
-    T[n,'FnB'] <- sum(T[1:n,'set']==2)/N
-    T[n,'DFn'] <- abs(T[n,'FnA']-T[n,'FnB'])
-  }
-  
-  c<-switch(toString(alpha),
-        "0.20" = 1.07,
-        "0.10" = 1.22,
-        "0.05" = 1.36,
-        "0.02" = 1.52,
-        1.63 # 0.01
-      ) * sqrt((2*N)/(N^2))
-  c(c=c,D=max(T[,'DFn']))
-}
+
 
 #
 # Run simulation
@@ -113,18 +114,15 @@ sim.mix.distr <- function(nvals) {
   
   xx <- seq(a-1, b+1, (b-a)/nvals)
   fx <- pdf(xx, p1, a, b, lambda)
-  gx <- gen.mix.distr(nvals, p1, a, b, lambda)
+  X  <- gen.mix.sample(nvals, p1, a, b, lambda)
   
   brk = 25 * floor((log(nvals,10) - 1))
-  plot.mix.distr(xx, fx, gx, a, b, lambda, breaks=brk)
+  plot.mix.sample(X, xx, fx, a, b, lambda, breaks=brk)
   
-  fset <- ceiling((length(fx)-length(gx))/2)
-  fx   <- fx[fset:(length(fx)-fset)]
-  
-  ks <- ks.mix.distr(fx,gx,alpha=0.05)
-  cat(sprintf("RR=]%0.5f,+Inf[, D'=%0.5f\n", ks['c'], ks['sup']))
-  
-  qqplot(nvals, gx,fx)
+  # FX <- cdf(xx,p1,a,b,lambda)
+  # Y  <- gen.mix.distr(xx,p1,a,b,lambda)
+  # ks.test(FX,Y,alternative='two.sided')
+  # qqplot(nvals, FX, Y)
 }
 
 
